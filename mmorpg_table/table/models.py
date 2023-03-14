3194f12691c109694d6966c728e6ed40136827fc
Bakiev75_New <bakiev75@mail.ru>
2023-03-14 23:44:29 +0500

    Создание 4-х пустых моделей

--- mmorpg_table/table/models.py
+++ mmorpg_table/table/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Users(models.Model):
+    """Класс Users для создания экземпляров пользователей портала"""
+    pass
+
+
+class Ads(models.Model):
+    """Класс Ads для создания экземпляров объявлений"""
+    pass
+
+
+class Reviews(models.Model):
+    """Класс Reviews для создания откликов пользователей на объявления"""
+    pass
+
+
+class Categories(models.Model):
+    """Класс Categories для создания категорий, к которым относятся объявления"""
+    pass
